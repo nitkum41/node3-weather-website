5f9391dc25690386b35146fbfe3002e67336b1b7
Nitesh Kumar <nitkum836@gmail.com>
2021-08-09 14:15:13 +0530

    changes in forecast file

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -15,7 +15,8 @@ const forecast=(longitude,latitude,callback)=>{
             }
             else{
         
-                callback(undefined," It is currently "+body.current.temperature+"degrees out. There is "+ body.current.precip+" % chance of rain.")
+                callback(undefined,body.current.weather_descriptions[0] + ".It is currently "+body.current.temperature+" degrees out.it feels like " +
+                body.current.feelslike + " degrees out. The humidity is " + body.current.humidity + "%")
             }
         })
 
